7980b0f5ee62d6a5434a8b8401b81ba7c170cdeb
deepak3005 <61706251+deepak3005@users.noreply.github.com>
2020-12-23 10:22:25 +0530

    Update Tower of Hanoi.java

--- Tower of Hanoi.java
+++ Tower of Hanoi.java
@@ -1,1 +1,11 @@
-// Code for tower of hanoi problem.
+// Code for Tower of Hanoi problem.
+
+void TOH(int n, char BEG, char AUX, char END)
+{
+  if(n>=1)
+	{
+		TOH(n-1,BEG,END,AUX);
+		System.out.println(BEG+" to "+END+"\n");
+		TOH(n-1,AUX,BEG,END);
+	}
+}
