aec43a42b500afe5fb30838bd3a9511e322d8e89
deepak3005 <61706251+deepak3005@users.noreply.github.com>
2020-12-29 11:26:49 +0530

    Single linked list implementation completed.

--- LinkedList.java
+++ LinkedList.java
@@ -1,5 +1,4 @@
 import java.util.*;
-import java.lang.*;
 
 class Node
 {
@@ -113,19 +112,49 @@ class LinkedList
 			System.out.println("Insertion not possible at position : "+pos);
 	}
 	
-	public void deleteAtPos()
-	{
-		
-	}
-	
 	public void deleteFirst()
 	{
-		
+		if(start==null)
+			System.out.println("List is already empty.");
+		else
+			start=start.getNext();
+			size--;
 	}
 	
 	public void deleteLast()
 	{
-		
+		Node t=start;
+		if(start==null)
+			System.out.println("List is already empty.");
+		else
+			for(int i=1;i<size-1;i++)
+			{
+				t=t.getNext();
+			}
+			t.setNext(null);
+			size--;
+	}
+	
+	public void deleteAtPos(int pos)
+	{
+		Node t=start;
+		Node t1=start;
+		if(start==null)
+			System.out.println("List is empty.");
+		else if(pos>size||pos<1)
+			System.out.println("Invalid position.");
+		else if(pos==1)
+			deleteFirst();
+		else if(pos==size)
+			deleteLast();
+		else
+			for(int i=1;i<pos-1;i++)
+			{
+				t=t.getNext();
+			}
+			t1=t.getNext();
+			t.setNext(t1.getNext());
+			size--;
 	}
 	
 	public void viewList()
@@ -151,6 +180,61 @@ public class Main
 {
 	public static void main(String args[])
 	{
-		
+		Scanner s = new Scanner(System.in);
+		LinkedList list = new LinkedList();
+		boolean flag=true;
+		while(flag)
+		{
+			System.out.println("");
+			System.out.println("1. Add item to the list at first");
+			System.out.println("2. Add item to the list at last");
+			System.out.println("3. Add item to the list at position");
+			System.out.println("4. Delete item from the list at first");
+			System.out.println("5. Delete item from the list at last");
+			System.out.println("6. Delete item from the list at position");
+			System.out.println("7. View List");
+			System.out.println("8. EXIT");
+			System.out.println("Enter your choice : ");
+			int choice = s.nextInt();
+			int position,value;
+			switch(choice)
+			{
+				case 1:
+					System.out.println("Enter value of item : ");
+					value = s.nextInt();
+					list.insertAtFirst(value);
+					break;
+				case 2:
+					System.out.println("Enter value of item : ");
+					value = s.nextInt();
+					list.insertAtLast(value);
+					break;
+				case 3:
+					System.out.println("Enter position of item : ");
+					position = s.nextInt();
+					System.out.println("Enter value of item : ");
+					value = s.nextInt();
+					list.insertAtPos(value, position);
+					break;
+				case 4:
+					list.deleteFirst();
+					break;
+				case 5:
+					list.deleteLast();
+					break;
+				case 6:
+					System.out.println("Enter position : ");
+					position = s.nextInt();
+					list.deleteAtPos(position);
+					break;
+				case 7:
+					list.viewList();
+					break;
+				case 8:
+					flag=false;
+					break;
+				default:System.out.println("Invalid choice");
+			}
+		}
 	}
 }
