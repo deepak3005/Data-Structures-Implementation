fa4cbead67d36d3e2023783f6b5cc18a1a979b16
deepak3005 <61706251+deepak3005@users.noreply.github.com>
2021-03-23 21:19:08 +0530

    Create Recursive_PreOrder_Traversal_Binary_Tree.java

---
+++ Binary Tree/Recursive_PreOrder_Traversal_Binary_Tree.java
@@ -0,0 +1,52 @@
+public class Recursive_PreOrder_Traversal_Binary_Tree
+{
+	private TreeNode root;
+	
+	private class TreeNode
+	{
+		private TreeNode left;
+		private TreeNode right;
+		private int data;
+		
+		public TreeNode(int data)
+		{
+			this.data = data;
+		}
+	}
+	
+	public void CreateBinaryTree()
+	{
+		TreeNode first = new TreeNode(1);
+		TreeNode second = new TreeNode(2);
+		TreeNode third = new TreeNode(3);
+		TreeNode fourth = new TreeNode(4);
+		TreeNode fifth = new TreeNode(5);
+		TreeNode sixth = new TreeNode(6);
+		
+		root = first;
+		first.left = second;
+		first.right = third;
+		second.left = fourth;
+		second.right = fifth;
+		third.left = sixth;
+	}
+	
+	public void preOrder(TreeNode root)
+	{
+		if(root == null)
+		{
+			return;
+		}
+		System.out.print(root.data + "    ");
+		preOrder(root.left);
+		preOrder(root.right);
+	}
+	
+	public static void main(String args[])
+	{
+		Recursive_PreOrder_Traversal_Binary_Tree m = new Recursive_PreOrder_Traversal_Binary_Tree();
+		System.out.println("PreOrder Traversal of Binary Tree :\n");
+		m.CreateBinaryTree();
+		m.preOrder(m.root);
+	}
+}
