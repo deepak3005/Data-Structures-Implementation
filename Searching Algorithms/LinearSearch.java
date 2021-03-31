6a10404d1265efa6c8e8c7e3926e47d29d67569f
deepak3005 <61706251+deepak3005@users.noreply.github.com>
2021-03-31 14:01:29 +0530

    Update LinearSearch.java

--- Searching Algorithms/LinearSearch.java
+++ Searching Algorithms/LinearSearch.java
@@ -1,1 +1,34 @@
+import java.util.Scanner;
 
+public class LinearSearch
+{
+	public void StartLinearSearch(int[] b,int capacity,int key)
+	{
+		for(int i=0;i<capacity;i++)
+		{
+			if(key==b[i])
+			{
+				System.out.print("Item found at index : "+i);
+				return;
+			}
+		}
+		System.out.print("Item NOT found !");
+	}
+	
+	public static void main(String args[])
+	{
+		LinearSearch m = new LinearSearch();
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter size of array : ");
+		int n = sc.nextInt();
+		System.out.print("Enter values : ");
+		int[] a = new int[n];
+		for(int i=0;i<n;i++)
+		{
+			a[i] = sc.nextInt();
+		}
+		System.out.print("Enter value of item to find : ");
+		int value = sc.nextInt();
+		m.StartLinearSearch(a,n,value);
+	}
+}
