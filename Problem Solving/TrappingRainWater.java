6f509fb1476c64918ae1aabbb6d64a23078faa61
deepak3005 <61706251+deepak3005@users.noreply.github.com>
2021-07-19 16:42:45 +0530

    Update TrappingRainWater.java

--- Problem Solving/TrappingRainWater.java
+++ Problem Solving/TrappingRainWater.java
@@ -1,78 +1,46 @@
-/* 
-
-Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it is able to trap after raining.
-
-Examples:  
-
-Output screen :
-
-Enter no of walls : 4
-
-Enter height of walls : 4 2 3 4
-
-Water Stored : 3 units
-
-*/
-
-
-import java.util.*;
+/*  Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it is able to trap after raining.
+	Input: arr[]   = {2, 0, 2}
+	Output: 2
+	
+*/	
 
 class TrappingRainWater
 {
-	public static int VolumeOfWaterStored(int[] a)
-	{
-		int totalVolume = 0;
-		int leftBoundary, rightBoundary, lower, waterVolume;
-		
-		for(int i=1;i<a.length-1;i++)
-		{
-			leftBoundary = a[i-1];
-			rightBoundary = a[i+1];
-			
-			for(int j=i-1;j>=0;j--)
-			{
-				if(a[j]>leftBoundary)
-				{
-					leftBoundary = a[j];
-				}
-			}
-			
-			for(int k=i+1;k<a.length;k++)
-			{
-				if(a[k]>rightBoundary)
-				{
-					rightBoundary = a[k];
-				}
-			}
-			
-			lower = Math.min(leftBoundary, rightBoundary);
-			
-			waterVolume = lower-a[i];
-			
-			if(waterVolume>0)
-			{
-				totalVolume = totalVolume + waterVolume;
-			}
-		}
-		
-		return totalVolume;
-	}
-	
-	public static void main(String args[])
-	{
-		Scanner sc = new Scanner(System.in);
-		
-		System.out.print("Enter no of walls : ");
-		int n = sc.nextInt();
-		
-		int[] arr = new int[n];
-		
-		System.out.print("\nEnter height of walls : ");
-		for(int i=0;i<n;i++)
-		{
-			arr[i] = sc.nextInt();
-		}
-		
-		System.out.println("\nWater Stored : "+VolumeOfWaterStored(arr)+" units");
-	}
+
+    public static int maxWater(int[] arr, int n)
+    {
+        // To store the maximum water that can be stored
+        int res = 0;
+
+        // For every element of the array except first and last element
+        for(int i = 1; i < n - 1; i++)
+        {
+            // Find maximum element on its left
+            int left = arr[i];
+            for(int j = 0; j < i; j++)
+            {
+                left = Math.max(left, arr[j]);
+            }
+
+            // Find maximum element on its right
+            int right = arr[i];
+            for(int j = i + 1; j < n; j++)
+            {
+                right = Math.max(right, arr[j]);
+            }
+
+            // Update maximum water value
+            res += Math.min(left, right) - arr[i];
+        }
+        return res;
+    }
+
+    public static void main(String[] args)
+    {
+        int[] arr = { 0, 1, 0, 2, 1, 0,
+                      1, 3, 2, 1, 2, 1 };
+        int n = arr.length;
+
+        System.out.print(maxWater(arr,n));
+    }
 }
